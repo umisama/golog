3b863762b5b715fdeb82e0a995ae37bfcbea2f3c
umisama <umisama@fe2o3.jp>
2014-02-08 17:52:39 +0900

    add LogTemplate member : ShortFileName, ShortFuncName

--- log.go
+++ log.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"runtime"
 	"strconv"
+	"strings"
 	"text/template"
 	"time"
 )
@@ -29,20 +30,22 @@ type Logger struct {
 type LogTemplate struct {
 	Time          string
 	FuncName      string
+	ShortFuncName string
 	FileName      string
+	ShortFileName string
 	LineNumber    string
 	Message       string
 }
 
 const (
 	LOG_FORMAT_SIMPLE   = "{{.Time}} : {{.Message}} \n"
-	LOG_FORMAT_STANDARD = "{{.Time}} {{.FileName}}:({{.LineNumber}}) : {{.Message}}\n"
-	LOG_FORMAT_POWERFUL = "{{.Time}} {{.FileName}}:{{.LineNumber}}({{.FuncName}}) : {{.Message}}\n"
+	LOG_FORMAT_STANDARD = "{{.Time}} {{.ShortFileName}}:({{.LineNumber}}) : {{.Message}}\n"
+	LOG_FORMAT_POWERFUL = "{{.Time}} {{.ShortFileName}}:{{.LineNumber}}({{.ShortFuncName}}) : {{.Message}}\n"
 )
 
 const (
-	TIME_FORMAT_DATE = "2006/1/2"
-	TIME_FORMAT_SEC = "2006/1/2 15:04:05"
+	TIME_FORMAT_DATE     = "2006/1/2"
+	TIME_FORMAT_SEC      = "2006/1/2 15:04:05"
 	TIME_FORMAT_MILLISEC = "2006/1/2 15:04:05.000"
 )
 
@@ -92,18 +95,24 @@ func (l *Logger) Printf(level int64, msg_fmt string, a ...interface{}) {
 }
 
 func (l *Logger) printer(str string) {
-	pc, file, line, ok := runtime.Caller(2)
+	pc, file_name, line_num, ok := runtime.Caller(2)
 	if !ok {
 		return
 	}
 
 	go func() {
+		func_name := runtime.FuncForPC(pc).Name()
+		func_name_s := func_name[strings.LastIndex(func_name, ".")+1:]
+		file_name_s := file_name[strings.LastIndex(file_name, "/")+1:]
+
 		d := &LogTemplate{
-			Time:       time.Now().Format(l.time_fmt),
-			FuncName:   runtime.FuncForPC(pc).Name(),
-			FileName:   file,
-			LineNumber: strconv.Itoa(line),
-			Message:    str,
+			Time:          time.Now().Format(l.time_fmt),
+			FuncName:      func_name,
+			ShortFuncName: func_name_s,
+			FileName:      file_name,
+			ShortFileName: file_name_s,
+			LineNumber:    strconv.Itoa(line_num),
+			Message:       str,
 		}
 
 		l.log_tmpl.Execute(l.dst, d)
